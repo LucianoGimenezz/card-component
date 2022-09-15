6d7503c9156b26cb3c89216dcf123238aa4ecc03
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-09-15 18:57:35 -0300

    :tada: [ADD] assets,configuracion

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <section className="card">
+        <div className="card__back">
+          <p>000</p>
+        </div>
+        <div className="card__front"></div>
+      </section>
+      <section className="form">
+        <form></form>
+      </section>
+    </div>
+  );
+}
+
+export default App;
