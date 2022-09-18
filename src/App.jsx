bd9c4212a55df8cafffd3494539b37993f135f45
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-09-18 20:36:59 -0300

    :lipstick: [UPDATE] card style
    Se termino el disenio de las dos tarjetas

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,22 @@ function App() {
         <div className="card__back">
           <p>000</p>
         </div>
-        <div className="card__front"></div>
+        <div className="card__front">
+          <div className="card___front__circles">
+            <div></div>
+            <div></div>
+          </div>
+          <div className="card__front__card-number">
+            <p>0000</p>
+            <p>0000</p>
+            <p>0000</p>
+            <p>0000</p>
+          </div>
+          <div className="card__front__info">
+            <p>Jane Appleseed</p>
+            <p>00/00</p>
+          </div>
+        </div>
       </section>
       <section className="form">
         <form></form>
