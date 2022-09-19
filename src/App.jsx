2bc240823156e6d2c613c43867a22eaa883c96b1
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-09-19 19:09:37 -0300

    :lipstick: [ADD] form design
    Se agrego el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,27 @@ function App() {
         </div>
       </section>
       <section className="form">
-        <form></form>
+        <form>
+          <label htmlFor="name">CARDHOLDER NAME</label>
+          <input type="text" name="name" placeholder="e.g. Jane Appleseed" />
+          <label htmlFor="cardNumber">CARD NUMBER</label>
+          <input
+            type="text"
+            name="cardNumber"
+            placeholder="e.g. 1234 5678 9123 0000"
+            maxLength="16"
+          />
+          <label htmlFor="cardDetails">
+            <p>EXP. DATE (MM/YY)</p>
+            <p>CVC</p>
+          </label>
+          <div className="form__details">
+            <input type="number" placeholder="MM" />
+            <input type="number" placeholder="YY" />
+            <input type="number" placeholder="e.g. 123" />
+          </div>
+          <button>Confirm</button>
+        </form>
       </section>
     </div>
   );
