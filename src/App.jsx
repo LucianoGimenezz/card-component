52bc5bcf98aa53b9841902bda69f91154a256cb6
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-09-22 13:57:30 -0300

    :lipstick: [UPDATE] card front component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [details, setDetails] = useState({
+    cardName: 'Jane Appleseed',
+    cardYear: '00',
+    cardMonth: '00',
+    cardCVC: '000',
+    cardNumber: '0000 0000 0000 0000',
+  });
+
+  const handleDetails = (e) => {
+    let chain = '';
+    let result = [];
+    for (let index = 0; index < e.target.value.length; index++) {
+      if (chain.length === 4) {
+        result.push(chain);
+        chain = '';
+      }
+      chain = chain.concat(e.target.value[index]);
+    }
+    result.push(chain);
+    setDetails({ ...details, cardNumber: result.join(' ') });
+  };
+
   return (
     <div className="app">
       <section className="card">
         <div className="card__back">
-          <p>000</p>
+          <p>{details.cardCVC}</p>
         </div>
         <div className="card__front">
           <div className="card___front__circles">
@@ -13,23 +36,33 @@ function App() {
             <div></div>
           </div>
           <div className="card__front__card-number">
-            <p>0000</p>
-            <p>0000</p>
-            <p>0000</p>
-            <p>0000</p>
+            <p>{details.cardNumber.slice(0, 4)}</p>
+            <p>{details.cardNumber.slice(5, 9)}</p>
+            <p>{details.cardNumber.slice(10, 14)}</p>
+            <p>{details.cardNumber.slice(15, 19)}</p>
           </div>
           <div className="card__front__info">
-            <p>Jane Appleseed</p>
-            <p>00/00</p>
+            <p>{details.cardName}</p>
+            <p>
+              {details.cardMonth}/{details.cardYear}
+            </p>
           </div>
         </div>
       </section>
       <section className="form">
         <form>
           <label htmlFor="name">CARDHOLDER NAME</label>
-          <input type="text" name="name" placeholder="e.g. Jane Appleseed" />
+          <input
+            onChange={(e) =>
+              setDetails({ ...details, cardName: e.target.value })
+            }
+            type="text"
+            name="name"
+            placeholder="e.g. Jane Appleseed"
+          />
           <label htmlFor="cardNumber">CARD NUMBER</label>
           <input
+            onChange={handleDetails}
             type="text"
             name="cardNumber"
             placeholder="e.g. 1234 5678 9123 0000"
@@ -40,9 +73,30 @@ function App() {
             <p>CVC</p>
           </label>
           <div className="form__details">
-            <input type="number" placeholder="MM" />
-            <input type="number" placeholder="YY" />
-            <input type="number" placeholder="e.g. 123" />
+            <input
+              onChange={(e) =>
+                setDetails({ ...details, cardMonth: e.target.value })
+              }
+              type="text"
+              placeholder="MM"
+              maxLength="2"
+            />
+            <input
+              onChange={(e) =>
+                setDetails({ ...details, cardYear: e.target.value })
+              }
+              type="text"
+              placeholder="YY"
+              maxLength="2"
+            />
+            <input
+              onChange={(e) =>
+                setDetails({ ...details, cardCVC: e.target.value })
+              }
+              type="text"
+              placeholder="e.g. 123"
+              maxLength="3"
+            />
           </div>
           <button>Confirm</button>
         </form>
